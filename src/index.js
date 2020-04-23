761367c8479897d92a0020d3f8a438a36ae7f08f
Chang  Gao <u5730383@anu.edu.au>
2020-04-23 12:29:57 +1000

    build form

--- src/index.js
+++ src/index.js
@@ -3,7 +3,78 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-);
+
+class BasicInfoForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: '',
+      gender:'',
+      age: null,
+      phone:'',
+      email:'',
+    };
+  }
+  formSubmition= (event) => {
+    event.preventDefault();
+    alert("You are submitting " + this.state.username);
+  }
+  formValidation = (event) => {
+    let nam = event.target.name;
+    let gender = event.target.gender;
+    let phone = event.target.phone;
+    let email = event.target.email;
+    let val = event.target.value;
+    if (nam === "age") {
+      if (!Number(val)) {
+        alert("Your age must be a number");
+      }
+    }
+    this.setState({[nam]: val});
+  }
+  render() {
+    return (
+      <form onSubmit={this.formSubmition}>
+      <p>Enter your name:</p>
+      <input
+        type='text'
+        name='name'
+        onChange={this.formValidation}
+      />
+
+      <p>Enter your gender:</p>
+       <input
+        type='text'
+        name='gender'
+        onChange={this.formValidation}
+      />
+      <p>Enter your age:</p>
+      <input
+        type='text'
+        name='age'
+        onChange={this.formValidation}
+      />
+      <p>Enter your phone:</p>
+        <input
+        type='text'
+        name='phone'
+        onChange={this.formValidation}
+      />
+       <p>Enter your email:</p>
+        <input
+        type='text'
+        name='email'
+        onChange={this.formValidation}
+      />
+       <p></p>
+       <input
+        type='submit'
+      />
+      <h1>Name:{this.state.name} Gender:{this.state.gender} Age:{this.state.age} Phone:{this.state.phone} Email:{this.state.email}</h1>
+
+      </form>
+    );
+  }
+}
+ReactDOM.render(<BasicInfoForm />, document.getElementById('root'));
+
